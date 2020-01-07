974735ba7dc9e813ffa74280099eaa03088cd454
roe <rockonedge@live.com>
2020-01-07 10:05:53 +0800

    added route contact

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import './assets/css/flatly.css'; 
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,5 +33,4 @@
   color: #42b983;
 }
 
-@import './assets/css/flatly.css'; 
 </style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="hero is-dark">
+
+    <div class="hero-body"><div class="container"> 
+      <nav class="level">  
+          <div class="level-item is-size-3" v-for='c in contacts' :key='c'>
+            <div v-html="c"></div>
+          </div>
+      </nav>
+    </div></div> 
+
+    </section> 
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        '<i class="fab fa-weixin"></i> 13524826926',
+        '<i class="far fa-envelope"></i> <a href="mailto:tom.tan@live.com">tom.tan@live.com</a>',
+        // '<i class="fas fa-mobile-alt"></i> +86 135-248-26926'
+      ]
+    };
+  },
+  methods: {
+    // join(items) {
+    //   return items ? items.join(" &middot; ") : "";
+    // }
+  }
+};
+</script>
+
